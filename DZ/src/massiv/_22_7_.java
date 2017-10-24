eb386d12b225ac35222c174ec62953bd3b1d421d
olejiksin <olejiksin@mail.ru>
2017-10-24 19:09:03 +0300

    add 27_7

--- DZ/src/massiv/_22_7_.java
+++ DZ/src/massiv/_22_7_.java
@@ -5,21 +5,22 @@ public class _22_7_ {
     public static void main(String[]args){
         Random r= new Random();
         Scanner sc=new Scanner(System.in);
-        int n,k=0;
+        int n,k=0,i=0;
         n=sc.nextInt();
         int[]a= new int [n];
-        for(int i=0; i<=n-1; i++){
+        for(i=0; i<=n-1; i++){
             a[i]=r.nextInt(n);
             System.out.print(a[i]+" ");
         }
-        for(int i=0; i<=n-1; n++){
-            for(int j=2; (a[i]/j)>0; j++){
-                if(j==a[i]) {
+        for(i=0;i<=n-1;i++) {
+            for (int j = 2; (a[i] / j) > 0; j++) {
+                if ((j  == a[i]) && (a[i]%2==1)) {
                     k++;
+                    System.out.println("   "+k+" ");
                 }
             }
         }
-        System.out.println(k);
+
     }
 
 }
