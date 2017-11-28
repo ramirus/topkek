a4d2aeea2ac7d4dcd5febed899ead58c90c349e1
olejiksin <olejiksin@mail.ru>
2017-11-28 22:38:54 +0300

    matrix

---
+++ DZ/src/OOP/RationalVector2D.java
@@ -0,0 +1,13 @@
+package OOP;
+//053
+public class RationalVector2D {
+    private double x;
+    private double y;
+    public RationalVector2D(){
+        double x=0;
+        double y=0;
+    }
+    public RationalVector2D(RationalVector2D rationalVector2D){
+        this.x
+    }
+}
